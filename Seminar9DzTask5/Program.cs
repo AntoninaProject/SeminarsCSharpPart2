33cbadacb3b830a4fb627e73af8a6ee8e94204a4
Antonina Petrova <ant2466@yandex.ru>
2023-02-18 19:58:59 +0300

    add Seminar 9 Dz Task5

---
+++ Seminar9DzTask5/Program.cs
@@ -0,0 +1,9 @@
+﻿// 5. Найти сумму цифр числа
+int x = int.Parse(Console.ReadLine());
+int sum=0;
+for (int i=3; i>=0; i--)
+{
+    sum+=x/(int)Math.Pow(10.0,i);
+    x=x%(int)Math.Pow(10.0,i);
+}
+Console.WriteLine(sum);
